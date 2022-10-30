c7055dc1fcba378767a83d484d7473eceb9bc6ca
BhuvanSA <93479180+BhuvanSA@users.noreply.github.com>
2022-10-30 20:30:37 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+typedef struct node
+{
+	int number;
+	struct node *next;
+}
+node;
+
+int main(void)
+{
+	node* list = NULL;
+	for (node *tmp = list; tmp != NULL; tmp = tmp-> next) 
+
+}
