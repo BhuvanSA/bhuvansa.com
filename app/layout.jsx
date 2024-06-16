35c56b9add202dc183baf3f4c8e555d2c0eeb737
BhuvanSA <bhuvansaraj2003@gmail.com>
2024-06-16 13:46:05 +0530

    Added analytics and speed insights

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 import "./globals.css";
 import { Outfit } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 // components
 import Header from "@/components/Header";
@@ -20,6 +22,8 @@ export default function RootLayout({ children }) {
         <ThemeProvider attribute="class" defaultTheme="light">
           <Header />
           {children}
+          <Analytics />
+          <SpeedInsights />
           <Footer />
         </ThemeProvider>
       </body>
