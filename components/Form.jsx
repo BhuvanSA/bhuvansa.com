5375dbdd91f28b9c6bb4890edb2550e6e36bbd92
BhuvanSA <bhuvansaraj2003@gmail.com>
2024-09-09 17:11:48 +0530

    refactor: Improve form submission status handling and messaging

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
@@ -5,42 +7,76 @@ import { User, MailIcon, ArrowRightIcon, MessageSquare } from "lucide-react";
 import { sendMail } from "@/lib/mail";
 
 const Form = () => {
-  const send = async (formData) => {
-    "use server";
-    await sendMail(
-      formData.get("name"),
-      formData.get("email"),
-      formData.get("message"),
-    );
-  };
+  const [status, setStatus] = useState(1);
+  async function handleSubmit(formData) {
+    setStatus(2);
+    try {
+      await sendMail(formData);
+      setStatus(3);
+    } catch (error) {
+      setStatus(4);
+    }
+  }
   return (
-    <form className="flex flex-col gap-y-4" action={send}>
-      {/* input */}
-      <div className="relative flex items-center">
-        <Input type="name" id="name" name="name" placeholder="Name" required />
-        <User size={20} className="absolute right-6" />
-      </div>
-      {/* input */}
-      <div className="relative flex items-center">
-        <Input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Email"
-          required
-        />
-        <MailIcon size={20} className="absolute right-6" />
-      </div>
-      {/* textarea */}
-      <div className="relative flex items-center">
-        <Textarea placeholder="Type Your Message" name="message" required />
-        <MessageSquare size={20} className="absolute right-6 top-4" />
-      </div>
-      <Button type="submit" className="flex max-w-[166px] items-center gap-x-1">
-        Let's Talk
-        <ArrowRightIcon size={20} className="ml-2" />
-      </Button>
-    </form>
+    <>
+      {status <= 2 && (
+        <form className="flex flex-col gap-y-4" action={handleSubmit}>
+          {/* input */}
+          <div className="relative flex items-center">
+            <Input
+              type="name"
+              id="name"
+              name="name"
+              placeholder="Name"
+              required
+            />
+            <User size={20} className="absolute right-6" />
+          </div>
+          {/* input */}
+          <div className="relative flex items-center">
+            <Input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email"
+              required
+            />
+            <MailIcon size={20} className="absolute right-6" />
+          </div>
+          {/* textarea */}
+          <div className="relative flex items-center">
+            <Textarea placeholder="Type Your Message" name="message" required />
+            <MessageSquare size={20} className="absolute right-6 top-4" />
+          </div>
+          {status === 1 && (
+            <Button
+              type="submit"
+              className="flex max-w-[166px] items-center gap-x-1"
+            >
+              Let's Talk
+              <ArrowRightIcon size={20} className="ml-2" />
+            </Button>
+          )}
+          {status === 2 && (
+            <Button
+              type="submit"
+              className="flex max-w-[166px] items-center gap-x-1"
+            >
+              Sending...
+              <ArrowRightIcon size={20} className="ml-2" />
+            </Button>
+          )}
+        </form>
+      )}
+      {status === 3 && (
+        <p className="text-green-500">Message sent successfully!</p>
+      )}
+      {status === 4 && (
+        <p className="text-red-500">
+          Failed to send the message. Please try again.
+        </p>
+      )}
+    </>
   );
 };
 
