d21c9f9e112f0f9b7dcd8441651ff13e4779c691
BhuvanSA <bhuvansa@icloud.com>
2025-11-27 01:43:46 +0530

    feat: Add umami analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,22 +11,30 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Bhuvan S A",
-  description: "Portfolio of Bhuvan S A",
+	title: "Bhuvan S A",
+	description: "Portfolio of Bhuvan S A",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={outfit.className}>
-        <ThemeProvider attribute="class" defaultTheme="light">
-          <Header />
-          {children}
-          <Analytics />
-          <SpeedInsights />
-          <Footer />
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<head>
+				{/* Umami Analytics */}
+				<script
+					defer
+					src="https://cloud.umami.is/script.js"
+					data-website-id="cdafa8d1-83ba-4f27-b0b9-34ca4bc5eb08"
+				></script>
+			</head>
+			<body className={outfit.className}>
+				<ThemeProvider attribute="class" defaultTheme="light">
+					<Header />
+					{children}
+					<Analytics />
+					<SpeedInsights />
+					<Footer />
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
